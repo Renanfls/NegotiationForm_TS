075cc8be871c771c6dcfc0f8e8ae96bc6dcb266d
Renanfls <contato.renanfabricio@gmail.com>
2023-08-30 21:02:42 -0300

    metodo limpaFormulario

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -11,17 +11,25 @@ export class NegociacaoController {
     this.inputValor = document.querySelector('#valor');
   }
 
-  adiciona() {
+  adiciona(): void {
     const negociacao = this.criaNegociacao();
     console.log(negociacao);
+    this.limpaFormulario();
   }
   
-  criaNegociacao() {
+  criaNegociacao(): Negociacao {
     const exp = /-/g;
     const date = new Date(this.inputData.value.replace(exp, ','));
     const quantidade = parseInt(this.inputQuantidade.value);
     const valor = parseFloat(this.inputValor.value);
     return new Negociacao(date, quantidade, valor);
+  }
+
+  limpaFormulario(): void {
+    this.inputData.value = '';
+    this.inputQuantidade.value = '';
+    this.inputValor.value = '';
+    this.inputData.focus();
   }
 }
 
